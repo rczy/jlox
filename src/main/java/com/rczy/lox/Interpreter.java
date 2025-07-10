463c75f42019351723a3c1dc2cecf6eb42617d78
rczy <berczy.balint@gmail.com>
2025-07-10 11:56:19 +0200

    Control flow: logical expressions ('or', 'and').

--- src/main/java/com/rczy/lox/Interpreter.java
+++ src/main/java/com/rczy/lox/Interpreter.java
@@ -135,7 +135,6 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitLogicalExpr(Expr.Logical expr) {
         Object left = evaluate(expr.left);
-
         if (expr.operator.type == TokenType.OR) {
             if (isTruthy(left)) return left;
         } else {
