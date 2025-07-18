4332f1f4cb7b888f23afa059a89a5b692bf0496c
rczy <berczy.balint@gmail.com>
2025-07-18 15:32:05 +0200

    Classes: creating instances.

---
+++ src/main/java/com/rczy/lox/LoxInstance.java
@@ -0,0 +1,14 @@
+package com.rczy.lox;
+
+public class LoxInstance {
+    private LoxClass klass;
+
+    LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<instance of %s>", klass.name);
+    }
+}
