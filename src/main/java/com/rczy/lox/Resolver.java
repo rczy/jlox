5a523f3164e77b28ff12d6e5851dbbff08a1ecd9
rczy <berczy.balint@gmail.com>
2025-07-22 21:50:04 +0200

    Inheritance: super and subclasses, method inheritance.

--- src/main/java/com/rczy/lox/Resolver.java
+++ src/main/java/com/rczy/lox/Resolver.java
@@ -88,6 +88,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         declare(stmt.name);
         define(stmt.name);
 
+        if (stmt.superclass != null) {
+            if (stmt.name.lexeme.equals(stmt.superclass.name.lexeme)) {
+                Lox.error(stmt.superclass.name, "A class cannot inherit from itself.");
+            }
+            resolve(stmt.superclass);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
 
